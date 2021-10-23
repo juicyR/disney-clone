95be0d3476000499fc88c33d734b62dae05c2ebf
juicyR <matovureginald@gmail.com>
2021-10-23 11:19:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello Babushka! Why won't my monitor work? 😡🤬</h2>      
+    </div>
+  );
+}
+
+export default App;
